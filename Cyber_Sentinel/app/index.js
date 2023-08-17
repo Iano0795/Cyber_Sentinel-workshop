a35c34cb1bea17d9875c7418fa0e566b79394b9b
Ian <92158188+Iano0795@users.noreply.github.com>
2023-08-17 06:54:44 +0000

    Started working on this workshop today

---
+++ Cyber_Sentinel/app/index.js
@@ -0,0 +1,11 @@
+import {View, Text, SafeAreaView, ScrollView} from 'react-native';
+
+const app = () => {
+    return (
+        <SafeAreaView>
+            <ScrollView>
+            <Text>CYBER SENTINEL WORKSHOP</Text>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
