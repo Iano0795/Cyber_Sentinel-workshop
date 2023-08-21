5e45f071ff9e51a97c7c7190e8d30c1156e6693b
Ian <92158188+Iano0795@users.noreply.github.com>
2023-08-21 07:30:12 +0000

    did some few changes

---
+++ expo_app/app/index.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import {View, ScrollView, SafeAreaView, Text} from 'react-native';
+import {Stack, useRouter} from 'expo-router';
+
+import {COLORS, icons, images, SIZES} from '../constants';
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+
+
+const app = () => {
+    const router = useRouter();
+    return (
+        <SafeAreaView style={{flex: 1, backgroundColor: COLORS.lightWhite}}>
+            <Stack.Screen 
+                options={{
+                    headerStyle: {backgroundColor: COLORS.lightWhite},
+                    headerShadowVisible: false,
+                    headerLeft: () => {
+                        <ScreenHeaderBtn iconurl={icons.menu} dimension="60%"/>
+                    },
+                    headerRight: () => {
+                        <ScreenHeaderBtn iconurl={images.profile} dimension="100%"/>
+                    },
+                    headerTitle: ""
+            }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex: 1, padding: SIZES.medium}}>
+                    <Welcome/>
+                    <Popularjobs/>
+                    <Nearbyjobs/>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
+    );
+}
