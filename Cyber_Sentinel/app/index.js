ae7413a947ef3a43259c8cf3c3ba14e0cc19995b
Ian <92158188+Iano0795@users.noreply.github.com>
2023-08-22 08:18:55 +0000

    added some folders

--- Cyber_Sentinel/app/index.js
+++ Cyber_Sentinel/app/index.js
@@ -1,52 +1,21 @@
+import {useState} from 'react';
 import {View, Text, SafeAreaView, ScrollView, TextInput, StyleSheet, Button, TouchableOpacity} from 'react-native';
 
-const styles = StyleSheet.create(
-    {
-        input_style:{
-            color: 'white',
-            backgroundColor: 'grey',
-            padding: 10,
-            margin: 10,
-            width: 100,
-            height: 30
-        },
-        heading:{
-            textAlign:'center',
-            fontSize: 40,
-            color: 'white'
-        },
-        container:{
-            width: '100%',
-            height: '100%'
-        },
-        form_container:{
-            flex:0.5,
-            backgroundColor: 'red',
-            width: '100%',
-        }
-    }
-)
+import {COLORS, icons, images, SIZES} from '../constants';
+import {Nearbyjobs, Popularjobs, ScreenHeaderBtn, Welcome} from '../components';
+import { Stack } from 'expo-router';
+
 
 const app = () => {
     return (
-        <View style={{backgroundColor: 'darkblue'}}>
-            <View style={styles.container}>
-                <Text style={styles.heading}>CYBER SENTINEL</Text>
-                <View style={styles.form_container}>
-                    <Text>Username/Email</Text>
-                    <TextInput style={styles.input_style} defaultValue='Username'></TextInput>
-                    <Text>Password</Text>
-                    <TextInput style={styles.input_style}defaultValue='Password'></TextInput>
-                    <Button 
-                        title='Login'
-                        style={{width: '30%', height: '10%'}}
-                    />
-                </View>
-                <View style={styles.form_container}>
-                    <Text>Forgot password?</Text>
-                </View>
-            </View>
-        </View>
+        <SafeAreaView>
+            <Stack.screen
+                options={{
+                    headerStyle: {background:COLORS.lightWhite}
+                    headerShadowVisible
+                }}
+            />
+        </SafeAreaView>
     );
 }
 
---
+++ Cyber_Sentinel/components/home/welcome/Welcome.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import { View, Text, Image, TextInput, FlatList, TouchableOpacity} from 'react-native'
+import {useRouter} from 'expo-router';
+
+import styles from './welcome.style';
+import {icons, SIZES} from '../../../constants';
+
+const jobTypes = ["Full-time", "Part-time", "Contractor"]
+
+const Welcome = () => {
+  const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-item')
+
+  return (
+    <View>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Ian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder='What are you looking for?'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({item}) => {
+            <TouchableOpacity 
+              style={styles.tab(activeJobType, item)}
+              onPress={() => {
+                setActiveJobType(item);
+                router.push(`/search/${item}`)
+              }}
+            >
+              <Text style={styles.tabText(activeJobType, item)}>{item}</Text>
+            </TouchableOpacity>
+          }}
+          keyExtractor={item => item}
+          contentContainerStyle={{columnGap: SIZES.small}}
+          horizontal
+        />
+      </View>
+    </View>
+  )
+}
+
+export default Welcome
